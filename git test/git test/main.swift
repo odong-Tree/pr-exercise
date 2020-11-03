4b36896eea28a5d8a74a57f18668ca51cf251462
odong-Tree <ss12357@naver.com>
2020-11-03 18:32:34 +0900

    odongtree

---
+++ git test/git test/main.swift
@@ -0,0 +1,19 @@
+//
+//  main.swift
+//  git test
+//
+//  Created by 김동빈 on 2020/11/03.
+//
+
+import Foundation
+
+print("Hello, World!")
+print("odongTree에게 충돌을")
+
+func multiplication(num: Int){
+<<<<<<< HEAD
+    print("구구단을 빠르게 외자")
+=======
+    print("구구단을 느리게 외자!")
+>>>>>>> 0d2241159637ec14c9480de0a7634ce03eea0b8d
+}
